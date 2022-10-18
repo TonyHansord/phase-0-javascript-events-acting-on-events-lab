2a9f5793d385156ff1031c8ee728b53ea9e0a186
TonyHansord <59306359+TonyHansord@users.noreply.github.com>
2022-10-18 18:19:27 +1100

    added left functionality

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = getElementById('dodger');
+
+function moveDodgerLeft() {
+
+const leftNumbers = dodger.style.left.replace("px", "");
+
+const left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+dodger.style.left = `${left - 1}px`;
+
+}
+
+}
+
+function moveDodgerRight() {
+
+}
+
+document.addEventListener("keydown" function(event) {
+
+if (event.key === "ArrowLeft") {
+
+moveDodgerLeft();
+
+}
+
+if (event.key === "ArrowRight") {
+
+moveDodgerRight();
+
+}
+
+}
+);
