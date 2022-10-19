13dcfe5ca1492a030ba07f9e3526ce1cccee107e
Anthony Hansord <tonyhansorddev@gmail.com>
2022-10-19 19:52:45 +1100

    finished assignment with test passing

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 // Your code here
-const dodger = getElementById('dodger');
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "lightBlue";
 
 function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
 
-const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-const left = parseInt(leftNumbers, 10);
-
-if (left > 0) {
-dodger.style.left = `${left - 1}px`;
-
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
 }
 
+function moveDodgerRight(position) {
+  const leftNumbers = dodger.style.left.replace("px", "");
+
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
 }
 
-function moveDodgerRight() {
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
 
-}
-
-document.addEventListener("keydown" function(event) {
-
-if (event.key === "ArrowLeft") {
-
-moveDodgerLeft();
-
-}
-
-if (event.key === "ArrowRight") {
-
-moveDodgerRight();
-
-}
-
-}
-);
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
